0840d692105a8aa2ff977606653fc4d1619249d2
jmBSU <joema@u.boisestate.edu>
2021-02-24 17:53:04 -0700

    Added basic sd card code

--- maskTest/src/main.cpp
+++ maskTest/src/main.cpp
@@ -5,14 +5,35 @@
 #include <math.h> 
 #include <MAX30105.h>
 #include <Adafruit_MLX90614.h>
+//#include <EEPROM.h> to have incremental log number
 
 #define bluetooth Serial1 //Define bluetooth to serial1
 #define usb Serial //usb to serial
 
+const int CHIP_SELECT = 10;
+const int MAX_SD_RETRY = 5;
+
+bool SD_FOUND = false;
+File log;
 
 void setup() {
   bluetooth.begin(9600);
   usb.begin(9600);
+
+  //Tries to start SD card
+  for(int i = 0; i < MAX_SD_RETRY; i++){
+    if(!SD.begin(CHIP_SELECT)){
+      delay(1000);
+    }else{
+      SD_FOUND = true;
+      break;
+    }
+  }
+
+  log = SD.open(strcpy("1_","log.txt"), FILE_WRITE); //Will probs change prefix with dynamic number via EEPROM
+
+  log.close();
+
 }
 
 void loop() {
@@ -21,7 +42,13 @@ void loop() {
     usb.write(bluetooth.read());
   }
 
+  log = SD.open(strcpy("1_","log.txt"), FILE_WRITE);
+  log.println("HELLO THERE");
+  log.close();
+
   if(usb.available()){
     bluetooth.write(usb.read());
   }
+
+
 }
